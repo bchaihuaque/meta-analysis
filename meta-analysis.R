b83b22e369964a9da4bcd25628ffe14a03f43165
bchaihuaque <bchaihuaque@pucp.pe>
2022-06-03 16:48:27 -0500

    03/06/2022 16:48 --PC desktop

--- meta-analysis.R
+++ meta-analysis.R
@@ -4,7 +4,16 @@
 # --- Fecha       :     03/06/2022                                          ---#
 # -----------------------------------------------------------------------------#
 
-# Lectura de paquetes
+# 1. CONFIGURACIÓN INICIAL ====
+# ??? 1.1 Initial settings
+rm(list = ls())
+Sys.getenv("USERNAME")
+# https://stackoverflow.com/questions/10164719/find-windows-user-name-within-r
+if (Sys.getenv("USERNAME") == "bchai"){
+  setwd("D:/Doctorado/Semestre I/Metodología de la Investigación/meta-analysis")
+}
+getwd()
+# ??? packages installation
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[,"Package"])]
   if (length(new.pkg))
@@ -13,3 +22,5 @@ ipak <- function(pkg){
 } 
 packages <- c("metafor", "esc", "readxl")
 ipak(packages)
+# ipak no requiere llamar a las librerias
+# Información complementaria: https://www.metafor-project.org/doku.php
