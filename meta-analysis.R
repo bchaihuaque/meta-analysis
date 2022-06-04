3a2020240f8e7e0d32dde639e25a0d5039afd63a
Bruno Chaihuaque <bchaihuaque@pucp.pe>
2022-06-04 02:42:32 -0500

    ubuntu 04/06/2022 2:42

--- meta-analysis.R
+++ meta-analysis.R
@@ -4,13 +4,16 @@
 # --- Fecha       :     03/06/2022                                          ---#
 # -----------------------------------------------------------------------------#
 
-# 1. CONFIGURACIÓN INICIAL ====
+# 1. CONFIGURACI?N INICIAL ====
 # ??? 1.1 Initial settings
 rm(list = ls())
 Sys.getenv("USERNAME")
 # https://stackoverflow.com/questions/10164719/find-windows-user-name-within-r
+# if else else if
 if (Sys.getenv("USERNAME") == "bchai"){
-  setwd("D:/Doctorado/Semestre I/Metodología de la Investigación/meta-analysis")
+  setwd("D:/Doctorado/Semestre I/Metodolog?a de la Investigaci?n/meta-analysis")
+} else if (Sys.getenv("USERNAME") == "bruno"){
+  setwd("/home/bruno/Doctorado/ResMet1/Meta-Analysis")
 }
 getwd()
 # ??? packages installation
@@ -23,7 +26,7 @@ ipak <- function(pkg){
 packages <- c("metafor", "esc", "readxl")
 ipak(packages)
 # ipak no requiere llamar a las librerias
-# Información complementaria: https://www.metafor-project.org/doku.php
+# Informaci?n complementaria: https://www.metafor-project.org/doku.php
 # ??? data upload
 ## Ejemplo 1: carga de la base de datos si tenemos cada Variable Dependiente (VD)
 ## en una hoja distinta de excel
@@ -31,20 +34,20 @@ ipak(packages)
 baseMA <- read_excel('', 
                      # hay que cambiar en sheet cada hoja sobre la VD
                      sheet = 'Reincidencia', col_names = TRUE)
-## Hacer lo mismo para cada pestaña
+## Hacer lo mismo para cada pesta?a
 
 ## Ejemplo 2: Carga de la base de datos si tenemos una sola hoja excel y una variable con 
 ## las VD del estudio
 baseMA <- read_excel('')
 baseMAreinci <- baseNA[baseMA$VD == 'Reincidencia',]
 
-## se cargan varias columnas, pero interesa la D "Tamaño del efecto" y como la 
-## variabilidad del tamaño del efecto
+## se cargan varias columnas, pero interesa la D "Tama?o del efecto" y como la 
+## variabilidad del tama?o del efecto
 
 ## Ejemplo aplicado
 baseMAWoS <- baseMA[baseMA$Buscador=='WoS',]
-## Ejemplo completa acá: http://www.simonqueenborough.info/R/specialist/meta-analysis
-## Cálculo de d y variabilidad para comparación de medias estandarizadas
+## Ejemplo completa ac?: http://www.simonqueenborough.info/R/specialist/meta-analysis
+## C?lculo de d y variabilidad para comparaci?n de medias estandarizadas
 help(escalc)
 escalc(measure = 'SMD', 
        m1i = baseMA$MControlPost,
